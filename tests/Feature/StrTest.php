e62adbdbc73a149f0db08ce6a4534a7896ff03d8
Tyler Dakin <tyler@dakin.one>
2024-04-28 15:20:37 -0400

    Add APA tests

--- tests/Feature/StrTest.php
+++ tests/Feature/StrTest.php
@@ -129,11 +129,48 @@ describe('Str', function () {
             'i-love-title-case' => 'I Love Title Case',
             'i_love_title_case' => 'I Love Title Case',
             'iLoveTitleCase' => 'I Love Title Case',
-
         ];
 
         foreach ($testLib as $pre => $post) {
             expect(Str::headline($pre))->toBe($post);
+        }
+    });
+
+    test('apa', function () {
+        $testLib = [
+            'i need this in aPA case' => 'I Need This in Apa Case',
+
+            'tom and jerry' => 'Tom and Jerry',
+            'TOM AND JERRY' => 'Tom and Jerry',
+            'Tom And Jerry' => 'Tom and Jerry',
+
+            'back to the future' => 'Back to the Future',
+            'BACK TO THE FUTURE' => 'Back to the Future',
+            'Back To The Future' => 'Back to the Future',
+
+            'this, then that' => 'This, Then That',
+            'THIS, THEN THAT' => 'This, Then That',
+            'This, Then That' => 'This, Then That',
+
+            'bond. james bond.' => 'Bond. James Bond.',
+            'BOND. JAMES BOND.' => 'Bond. James Bond.',
+            'Bond. James Bond.' => 'Bond. James Bond.',
+
+            'self-report' => 'Self-Report',
+            'Self-report' => 'Self-Report',
+            'SELF-REPORT' => 'Self-Report',
+
+            'as the world turns, so are the days of our lives' => 'As the World Turns, So Are the Days of Our Lives',
+            'AS THE WORLD TURNS, SO ARE THE DAYS OF OUR LIVES' => 'As the World Turns, So Are the Days of Our Lives',
+            'As The World Turns, So Are The Days Of Our Lives' => 'As the World Turns, So Are the Days of Our Lives',
+
+            'to kill a mockingbird' => 'To Kill a Mockingbird',
+            'TO KILL A MOCKINGBIRD' => 'To Kill a Mockingbird',
+            'To Kill A Mockingbird' => 'To Kill a Mockingbird',
+        ];
+
+        foreach ($testLib as $pre => $post) {
+            expect(Str::apa($pre))->toBe($post);
         }
     });
 
