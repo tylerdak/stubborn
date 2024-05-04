832d7d3c166411dbb767765acb80f0468d121a79
Tyler Dakin <tyler@dakin.one>
2024-05-04 11:02:43 -0400

    Add test for layering mods

--- tests/Feature/ModTest.php
+++ tests/Feature/ModTest.php
@@ -32,3 +32,17 @@ test('camel, kebab, snake, and studly from normal spaces', function () {
     expect($success)->toBeTrue();
     expect($this->readResult('cases.php'))->toBe('myName my-name my_name MyName');
 });
+
+test('mods can be layered', function () {
+    $stub = $this->writeTest('layers', "{{ VAR::camel }} {{ VAR::camel::kebab }} {{ VAR::camel::kebab::snake }} {{ VAR::camel::kebab::snake::studly }} {{ VAR::camel::kebab::snake::studly::upper }}");
+
+    $success = Stub::from($stub)
+        ->to(__DIR__ . '/../Generated')
+        ->replace('VAR','My Name')
+        ->name('result_layers')
+        ->ext('php')
+        ->generate();
+
+    expect($success)->toBeTrue();
+    expect($this->readResult('layers.php'))->toBe('myName my-name my_name MyName MYNAME');
+});
